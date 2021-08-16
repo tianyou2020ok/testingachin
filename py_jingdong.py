7ab47795189022a08d145a8d2a1050182a8f2ce8
tianyou2020ok <79851625+tianyou2020ok@users.noreply.github.com>
2021-08-16 12:41:48 +0800

    Update py_jingdong.py

--- py_jingdong.py
+++ py_jingdong.py
@@ -1,4 +1,3 @@
-
 # 1.*
 # 2.all categories
 # 3.all categories and brand
@@ -9,15 +8,45 @@ conn = connect(host='localhost',port=3306,database='jing_dong',user='root',passw
 
 cs1 = conn.cursor()
 
-count = cs1.execute('select * from goods')
 
-print(count)
+print('1.查询所有数据')
+print('1.查询所有种类')
+print('1.查询所有种类和品牌')
+print('4.退出')
+select = int(input('请输入'))
 
-result = cs1.fetchall()
-print(result)
+while True:
+    #
+    if select == 1:
+        count = cs1.execute('select * from goods')
 
+        print('共有d%行数据' %count)
+        result = cs1.fetchall()
+        print(result)
+
+    #
+    if select == 2:
+        count = cs1.execute('select * from goods')
+
+        print('共有d%行数据' %count)
+        result = cs1.fetchall()
+        print(result)
+
+    #
+    if select == 3:
+        count = cs1.execute('select * from goods')
+
+        print('共有d%行数据' %count)
+        result = cs1.fetchall()
+        print(result)
+
+    #
+    if select == 4:
+        break
+
+    if select != 1 or select != 2 or select != 3 or select != 4:
+        print("输入有误，请重新输入")
 
 cs1.close()
 conn.close()
-
 
